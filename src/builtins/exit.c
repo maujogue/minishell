dfa72fc4c9a9951980df5e20a047e4b33c434252
Mathis Aujogue <maujogue@z1r5p4.42lyon.fr>
2023-03-06 13:33:25 +0100

    added exit and pwd first sketch

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,8 +6,13 @@
 /*   By: maujogue <maujogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 10:07:00 by maujogue          #+#    #+#             */
-/*   Updated: 2023/03/06 10:08:17 by maujogue         ###   ########.fr       */
+/*   Updated: 2023/03/06 13:30:18 by maujogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/minishell.h"
+
+void ft_exit(int code)
+{
+    exit(code);
+}
