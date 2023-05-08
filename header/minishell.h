3c29ebe840c9736243e7ece0efda8b4fbf0a358e
Axel Vaganay <avaganay@z1r5p4.42lyon.fr>
2023-05-08 10:09:42 +0200

    norminette

--- header/minishell.h
+++ header/minishell.h
@@ -6,7 +6,7 @@
 /*   By: avaganay <avaganay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 10:42:36 by avaganay          #+#    #+#             */
-/*   Updated: 2023/05/05 15:43:18 by avaganay         ###   ########.fr       */
+/*   Updated: 2023/05/08 10:06:38 by avaganay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,6 +83,10 @@ char		**ft_filetodouble(char **tab, char *file);
 int			ft_strlen_triple_char(char ***str);
 void		ft_fillstructpars(t_pars **pars, char **tabcmd);
 char		*ft_replace_var(t_all *all, char *cmd);
+void		ft_fillpars_infile(t_pars *pars, char *cmd);
+void		ft_fillpars_outfile(t_pars *pars, char *cmd);
+void		ft_fillpars_outfile_append(t_pars *pars, char *cmd);
+void		ft_fillpars_heredoc(t_pars *pars, char *cmd);
 
 /****************************SIGNALS********************************/
 
