af41e0f9fbe4ecd7bd7a468bee6cbdc0567cf4b4
Mathis Aujogue <maujogue@z1r4p5.42lyon.fr>
2023-05-25 13:30:11 +0200

    fixed header

--- header/pipex.h
+++ header/pipex.h
@@ -6,7 +6,7 @@
 /*   By: maujogue <maujogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 16:38:15 by maujogue          #+#    #+#             */
-/*   Updated: 2023/05/24 16:16:58 by maujogue         ###   ########.fr       */
+/*   Updated: 2023/05/25 13:28:48 by maujogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ void	free_files(t_all *all, int i);
 void	free_fd(void);
 void	free_each_pipe(t_pip *pip);
 void	free_exit(t_all *all, t_pip *pip, int i, char *message);
+void	free_pipex(t_all *all, t_pip *pip);
 
 /****************************EXEC********************************/
 
